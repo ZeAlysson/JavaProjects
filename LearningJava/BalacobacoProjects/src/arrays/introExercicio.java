1ee0ed4f4d4e6b27d01b5cb3412e2f90650f78b7
Alysson-Silva <alyssonlogar123@gmail.com>
2021-09-21 20:12:51 -0300

    Arrays adicionados

---
+++ LearningJava/BalacobacoProjects/src/arrays/introExercicio.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class introExercicio {
+    public static void main(String[] args) {
+        
+        Scanner entradaDados = new Scanner(System.in);
+
+        System.out.println("Insira quantas notas deseja adicionar: ");
+        int qtd_notas = entradaDados.nextInt();//ler a quantidade de notas do usuario
+
+        double[] notas = new double[qtd_notas];// cria um array com a quantidade de notas
+        
+        //ler as notas do usuario
+        for (int i = 0; i < notas.length; i++)
+        {
+            System.out.printf("Insira a nota[%d]: ", i+1);
+            notas[i] = entradaDados.nextDouble();
+        }
+
+        System.out.println(Arrays.toString(notas));//mostra os valores lidos no for acima
+
+        //calcula a soma das notas
+        double somaTotal = 0;
+        for (double nota : notas) 
+        {
+            somaTotal += nota;
+        }
+
+        //calcula e mostra a media das notas
+        System.out.println("Media das notas = " + (somaTotal/notas.length));
+
+        entradaDados.close();
+
+    }
+
+}
